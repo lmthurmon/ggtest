06d32a39b87af7480340e117d008e564908d9e8e
Landon Thurmon <landonthurmon1@gmail.com>
2020-02-17 20:46:13 -0600

    Add project files.

---
+++ ggtest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ggtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
